22fd4b7dfc60e3bb847241bea4a6b764b390cf8a
Nourk4441 <nourk4441@gmail.com>
2024-11-16 23:00:07 +0200

    task 11.1

---
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render, get_object_or_404
+from .models import Book
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    # Handle book editing logic
+    return render(request, 'bookshelf/edit_book.html', {'book': book})
+
+def index(request): return HttpResponse("Welcome to my book store.")
