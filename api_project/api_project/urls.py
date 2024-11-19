22c4c083854fa8797703c31b9334fdcd86e74eaf
Nourk4441 <nourk4441@gmail.com>
2024-11-19 20:43:10 +0200

    task 12.1

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from api.views import BookListCreateAPIView
+from django.urls import include, path
+from api.views import BookList
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/books", BookListCreateAPIView.as_view(), name="book_list_create"),
+    # path("api/books", BookListCreateAPIView.as_view(), name="book_list_create"),
+    path('books/', BookList.as_view(), name='book-list'),
+    # path('api/', include('api.urls'))
 ]
