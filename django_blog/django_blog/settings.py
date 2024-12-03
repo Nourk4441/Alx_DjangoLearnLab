748fdfe6da6362445157c416870699cc8dbabac4
Nourk4441 <nourk4441@gmail.com>
2024-12-03 18:18:25 +0200

    task 14.0

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -75,9 +75,17 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sys',
+        'USER': 'root',
+        'PASSWORD': 'Nour@2002',
+        'HOST': 'localhost',  # or the hostname of your MySQL server
+        'PORT': '3306',       # Default MySQL port
     }
 }
 
