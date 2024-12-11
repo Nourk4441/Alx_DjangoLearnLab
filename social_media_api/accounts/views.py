6bceb0d6f637a03b4698829e917f647a2d8ee805
Nourk4441 <nourk4441@gmail.com>
2024-12-11 12:41:41 +0200

    task 15.2

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -5,9 +5,8 @@ from .models import CustomUser
 from .serializers import UserSerializer
 from django.contrib.auth import authenticate
 from django.shortcuts import render
-from rest_framework import status, generics
+from rest_framework import status, permissions, generics
 from django.shortcuts import get_object_or_404
-from rest_framework.permissions import IsAuthenticated
 
 class RegisterView(APIView):
     def post(self, request):
@@ -30,7 +29,7 @@ class LoginView(APIView):
     
 class FollowUserView(generics.GenericAPIView):
     queryset = CustomUser.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, *args, **kwargs):
         user_to_follow = get_object_or_404(self.get_queryset(), id=self.kwargs.get('user_id'))
@@ -42,7 +41,7 @@ class FollowUserView(generics.GenericAPIView):
 
 class UnfollowUserView(generics.GenericAPIView):
     queryset = CustomUser.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, *args, **kwargs):
         user_to_unfollow = get_object_or_404(self.get_queryset(), id=self.kwargs.get('user_id'))
