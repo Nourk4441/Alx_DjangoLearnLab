fdbd6c241430751717cf420d9d9267b4cf6aac9b
Nourk4441 <nourk4441@gmail.com>
2024-12-11 12:40:36 +0200

    task 15.2

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -5,8 +5,9 @@ from .models import CustomUser
 from .serializers import UserSerializer
 from django.contrib.auth import authenticate
 from django.shortcuts import render
-from rest_framework import status, permissions
+from rest_framework import status, generics
 from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
 
 class RegisterView(APIView):
     def post(self, request):
@@ -27,22 +28,24 @@ class LoginView(APIView):
             return Response({'token': token.key}, status=status.HTTP_200_OK)
         return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
     
-class FollowUserView(APIView):
-    permission_classes = [permissions.IsAuthenticated]
+class FollowUserView(generics.GenericAPIView):
+    queryset = CustomUser.objects.all()
+    permission_classes = [IsAuthenticated]
 
-    def post(self, request, user_id):
-        user_to_follow = get_object_or_404(CustomUser, id=user_id)
+    def post(self, request, *args, **kwargs):
+        user_to_follow = get_object_or_404(self.get_queryset(), id=self.kwargs.get('user_id'))
         if user_to_follow == request.user:
             return Response({"error": "You cannot follow yourself."}, status=status.HTTP_400_BAD_REQUEST)
 
         request.user.following.add(user_to_follow)
         return Response({"message": f"You are now following {user_to_follow.username}."}, status=status.HTTP_200_OK)
 
-class UnfollowUserView(APIView):
-    permission_classes = [permissions.IsAuthenticated]
+class UnfollowUserView(generics.GenericAPIView):
+    queryset = CustomUser.objects.all()
+    permission_classes = [IsAuthenticated]
 
-    def post(self, request, user_id):
-        user_to_unfollow = get_object_or_404(CustomUser, id=user_id)
+    def post(self, request, *args, **kwargs):
+        user_to_unfollow = get_object_or_404(self.get_queryset(), id=self.kwargs.get('user_id'))
         if user_to_unfollow == request.user:
             return Response({"error": "You cannot unfollow yourself."}, status=status.HTTP_400_BAD_REQUEST)
 
