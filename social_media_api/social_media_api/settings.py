5d91e5a0f8478e2aa84cb6e63719c2d500328bfe
Nourk4441 <nourk4441@gmail.com>
2024-12-15 22:19:10 +0200

    task 15.4

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -51,6 +51,8 @@ INSTALLED_APPS = [
     'notifications',
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
